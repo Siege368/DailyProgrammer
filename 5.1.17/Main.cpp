dd3104a40729e732e28ca4d6aa477efff47686b3
Siege368 <siegeflank@gmail.com>
2017-05-01 19:58:44 -0400

    Bonus Challenge WIP

--- 5.1.17/Main.cpp
+++ 5.1.17/Main.cpp
@@ -4,6 +4,35 @@
 
 using namespace std;
 
+bool IsZeroSumSet(vector<int> v)
+{
+	if (v.size() == 1)
+	{
+		return v[0] == 0;
+	}
+	else
+	{
+		int sum = 0;
+		for (int i = 0; i < v.size(); i++)
+		{
+			sum += v[i];
+			if (sum > 0)
+				break;
+		}
+
+		if (sum == 0)
+			return true;
+
+		for (int i = 0; i < v.size(); i++)
+		{
+			vector<int> nv = v;
+			nv.erase(i);
+			return IsZeroSumSet(nv);
+		}
+	}
+	return false;
+}
+
 int main(int argc, char *argv[])
 {
 	vector<int> v;
@@ -12,6 +41,7 @@ int main(int argc, char *argv[])
 		v.push_back(atoi(argv[i]));
 	}// for
 
+	// Main Challenge
 	for (unsigned i = 0; i < v.size(); i++)
 	{
 		for (unsigned j = 0; j < v.size(); j++)
